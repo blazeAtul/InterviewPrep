c776fbe28896a725a7cd8628d23463082d4cf656
Atul Kumar <katul1602@gmail.com>
2024-08-24 16:08:06 +0530

    Adding smallestNumberArray

--- src/Questions/LargestNumberInArray.java
+++ src/Questions/LargestNumberInArray.java
@@ -33,4 +33,13 @@ public class LargestNumberInArray {
         }
         System.out.println(max);
     }
+    public static void smallestNumberUsingFor(int[] arr) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println(min);
+    }
 }
